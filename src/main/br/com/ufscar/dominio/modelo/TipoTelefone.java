9f58ed68ff9fc402f44555dd1a47af4df0e6539d
marcelohb <marcelobattaglion@bol.com.br>
2015-05-29 23:57:36 -0300

    Criação dos testes com Telefone

---
+++ src/main/br/com/ufscar/dominio/modelo/TipoTelefone.java
@@ -0,0 +1,29 @@
+package br.com.ufscar.dominio.modelo;
+
+public enum TipoTelefone {
+	CELULAR('C'),
+	FAX('F'),
+	RESIDENCIAL('R'),
+	COMERCIAL('M');
+	
+	private char tipo; 
+	
+	TipoTelefone(char tipo) {
+		this.tipo = tipo;
+	}
+	
+	public static Object obterPeloCodigo(char tipo) {
+		switch (tipo) {
+		case 'C':
+			return CELULAR;
+		case 'F':
+			return FAX;
+		case 'R':
+			return RESIDENCIAL;
+		case 'M':
+			return COMERCIAL;
+		default:
+			return null;
+		}
+	}
+}
